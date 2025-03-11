f98df36dbaa9a28d23773d4001ea0c47c475ff66
m-peckus <martinpeckus@gmail.com>
2025-03-11 12:15:48 +0100

    commit 10 Modification GUI

--- app/app.py
+++ app/app.py
@@ -3,10 +3,12 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-#from data_fetcher import get_data
+from data_fetcher import get_data
 from nested_eu_data import *
 
 app = FastAPI()
+
+# Load HTML templates
 templates = Jinja2Templates(directory="/home/mpeckus/eu_capitals_backend/app/templates")
 
 @app.get("/", response_class=HTMLResponse)
@@ -16,12 +18,23 @@ async def home(request: Request):
 @app.post("/", response_class=HTMLResponse)
 async def fetch_info(request: Request, city: str = Form(...)):
     city = city.strip().title() # Normalize input
+
     if city in eu_data_extended:
+        country = eu_data_extended[city]["country"]
+        currency = eu_data_extended[city]["currency_name"]
+
+        # Fetch full data (population, exchange rate, weather, etc.)
+        data = get_data(city, country)
+
         result = {
             "city_found": True,
             "city": city,
-            "country": eu_data_extended[city]["country"],
-            "currency": eu_data_extended[city]["currency_name"]
+            "country": country,
+            "currency": currency,
+            "city_population": data["city_population"],
+            "country_population": data["country_population"],
+            "exchange_rate": data["exchange_rate"] if currency != "Euro" else None,
+            "temperature": data["temperature"],
         }
     else:
         result = {"city_found": False, "city": city}
